9b26916709c436e71627c618f17e8bf2bb3eafe6
EfeAdzhar <efeadzhar@gmail.com>
2021-10-17 00:24:05 +0300

    writing third task

--- hw_1_base_operations/src/main/java/ua/com/alevel/EndOfALesson.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/EndOfALesson.java
@@ -1,7 +1,13 @@
 package ua.com.alevel;
-
+import java.util.Scanner;
 public class EndOfALesson {
     public static void main(String[] args) {
+        EndOfALesson endOfALesson = new EndOfALesson();
 
+
+    }
+    public void lessonEndCounter() {
+        int lessonEndMinutes;
+        int lessonEndHours;
     }
 }
