e0733533a40ee553e324c873c5b5c70e41672487
EfeAdzhar <efeadzhar@gmail.com>
2021-10-19 21:42:59 +0300

    Making second task

--- hw_2_strings/src/main/java/ua/com/alevel/Test.java
+++
@@ -1,7 +0,0 @@
-package ua.com.alevel;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Testing");
-    }
-}
---
+++ hw_2_strings/src/main/java/ua/com/alevel/reversestring/StringMain.java
@@ -0,0 +1,4 @@
+package ua.com.alevel.reversestring;
+
+public class StringMain {
+}
