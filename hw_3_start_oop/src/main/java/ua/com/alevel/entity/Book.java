71fa427569aae9301756fbd3f80e4cc0c310e7fb
EfeAdzhar <efeadzhar@gmail.com>
2021-10-26 21:42:03 +0300

    Creating ENTITY

---
+++ hw_3_start_oop/src/main/java/ua/com/alevel/entity/Book.java
@@ -0,0 +1,50 @@
+package ua.com.alevel.entity;
+
+public class Book {
+    private int id;
+    private String BookTitle;
+    private String BookAuthor;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return BookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        BookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return BookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        BookAuthor = bookAuthor;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", BookTitle='" + BookTitle + '\'' +
+                ", BookAuthor='" + BookAuthor + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
