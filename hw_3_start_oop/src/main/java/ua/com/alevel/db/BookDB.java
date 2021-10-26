99c85f373d18fb0d0ff647535edd9c2c8e083647
EfeAdzhar <efeadzhar@gmail.com>
2021-10-26 23:13:44 +0300

    Commit 3. Adding Service, Control. Making DB

---
+++ hw_3_start_oop/src/main/java/ua/com/alevel/db/BookDB.java
@@ -0,0 +1,51 @@
+package ua.com.alevel.db;
+import ua.com.alevel.entity.Book;
+
+public class BookDB {
+    private final Book[] books;
+    private static BookDB instance;
+    private static int newBookId = 0;
+
+    public BookDB() {
+        books = new Book[100];
+    }
+
+    public static BookDB getInstance() {
+        if (instance == null) {
+            instance = new BookDB();
+        }
+        return instance;
+    }
+
+    public void create(Book book) {
+        book.setId(newBookId);
+        books[newBookId] = book;
+        newBookId++;
+    }
+
+    public void update(Book book) {
+        Book current = findById(book.getId());
+        current.setPrice(book.getPrice());
+        current.setBookTitle(book.getBookTitle());
+    }
+
+    public void delete(int id) {
+        for (int i = id; i < newBookId - 1; i++) {
+            books[i] = books[i + 1];
+            books[i].setId(books[i].getId() - 1);
+        }
+        newBookId--;
+    }
+
+    public Book findById(int id) {
+        return books[id];
+    }
+
+    public static int sizeOf() {
+        return newBookId;
+    }
+
+    public Book[] findAll() {
+        return books;
+    }
+}
