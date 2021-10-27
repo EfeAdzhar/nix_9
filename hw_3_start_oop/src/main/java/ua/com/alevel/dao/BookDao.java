50895a7cbb8e3bbb7f4e5fc96df28268a238252e
EfeAdzhar <efeadzhar@gmail.com>
2021-10-27 19:58:11 +0300

    Commit 4. Maded Dao and Service

--- hw_3_start_oop/src/main/java/ua/com/alevel/dao/BookDao.java
+++ hw_3_start_oop/src/main/java/ua/com/alevel/dao/BookDao.java
@@ -1,5 +1,29 @@
 package ua.com.alevel.dao;
+import ua.com.alevel.entity.Book;
+import ua.com.alevel.db.BookDB;
 
 public class BookDao {
+    public void create(Book book) {
+        BookDB.getInstance().create(book);
+    }
 
+    public void update(Book book) {
+        BookDB.getInstance().update(book);
+    }
+
+    public void delete(int id) {
+        BookDB.getInstance().delete(id);
+    }
+
+    public static int sizeOf() {
+        return BookDB.sizeOf();
+    }
+
+    public Book findById(int id) {
+        return BookDB.getInstance().findById(id);
+    }
+
+    public Book[] findAll() {
+        return BookDB.getInstance().findAll();
+    }
 }
--- hw_3_start_oop/src/main/java/ua/com/alevel/service/BookService.java
+++ hw_3_start_oop/src/main/java/ua/com/alevel/service/BookService.java
@@ -1,4 +1,33 @@
 package ua.com.alevel.service;
 
+import ua.com.alevel.dao.BookDao;
+import ua.com.alevel.db.BookDB;
+import ua.com.alevel.entity.Book;
+
 public class BookService {
+    private final BookDao bookDao = new BookDao();
+
+    public void create(Book book) {
+        BookDB.getInstance().create(book);
+    }
+
+    public void update(Book book) {
+        BookDB.getInstance().update(book);
+    }
+
+    public void delete(int id) {
+        BookDB.getInstance().delete(id);
+    }
+
+    public static int sizeOf() {
+        return BookDB.sizeOf();
+    }
+
+    public Book findById(int id) {
+        return BookDB.getInstance().findById(id);
+    }
+
+    public Book[] findAll() {
+        return BookDB.getInstance().findAll();
+    }
 }
